1d197b4b4cc27dbfac944e8551c8c976bacc0e36
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2024-01-22 23:04:19 -0500

    Add base for the Client GUI
    * We will use an MVC model
    * Add dependency Cryptography

---
+++ Client/src/main/java/local/silverton/client/Model.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package local.silverton.client;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class Model {
+    
+}

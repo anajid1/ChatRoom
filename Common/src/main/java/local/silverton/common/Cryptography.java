a18db194da9221f97f4183c05ddd63d90f25db26
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2024-01-22 21:50:22 -0500

    Create modules: Server, Client, Common

---
+++ Common/src/main/java/local/silverton/common/Cryptography.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package local.silverton.common;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class Cryptography {
+
+    
+}
---
+++ Server/src/main/java/local/silverton/server/Server.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package local.silverton.server;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Server!");
+    }
+}
